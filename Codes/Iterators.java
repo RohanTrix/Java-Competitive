618c22c8154c3c8c4b7ed6433362c0f009b23dc3
RohanTrix <rohanyeahyeah@gmail.com>
2020-10-05 11:19:40 +0530

    Committed changes

---
+++ Codes/Iterators.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList; 
+import java.util.Iterator; 
+
+public class Iterators
+{ 
+    public static void main() 
+    { 
+        ArrayList<Integer> al = new ArrayList<Integer>(); 
+  
+        for (int i = 0; i < 10; i++) 
+            al.add(i); 
+  
+        System.out.println(al); 
+  
+        // at beginning itr(cursor) will point to 
+        // index just before the first element in al 
+        Iterator itr = al.iterator(); 
+  
+        // checking the next element availabilty 
+        while (itr.hasNext()) 
+        { 
+            //  moving cursor to next element 
+            int i = (Integer)itr.next(); 
+  
+            // getting even elements one by one 
+            System.out.print(i + " "); 
+  
+            // Removing odd elements 
+            if (i % 2 != 0) 
+               itr.remove();  
+        } 
+        System.out.println();  
+        System.out.println(al); 
+    } 
+} 
