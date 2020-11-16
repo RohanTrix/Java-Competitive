157b69380a0038f6ddb1479ad062549f50060066
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-16 12:00:31 +0530

    Committed changes

---
+++ DSA/Graphs/DSU.java
@@ -0,0 +1,23 @@
+package Graphs;
+public class DSU 
+{
+    public static int parent[];
+
+    public DSU(int num)
+    {
+        parent = new int[num];
+    }
+    public int find( int i)
+    {
+        if(parent[i]== -1)
+        {
+            return i;
+        }
+        return find(parent[i]);
+    }
+    public void union(int a, int b)
+    {
+        int s1 = find(a);
+        int 
+    }
+}
