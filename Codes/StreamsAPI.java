cf368000da3c27c68aab4da6cf3484f2ae9eb8f1
RohanTrix <rohanyeahyeah@gmail.com>
2021-02-18 19:55:15 +0530

    Changes

---
+++ Codes/StreamsAPI.java
@@ -0,0 +1,25 @@
+
+import java.util.stream.*;
+@SuppressWarnings("all")
+public class StreamsAPI {
+    public static void main(String args[])
+    {
+        int numbers[] = {4, 1, 13, 90, 16, 2, 0};
+        //Minimum
+        int min = IntStream.of(numbers).min().getAsInt();
+        //Maximum
+        int max = IntStream.of(numbers).max().getAsInt();
+        //Sum
+        int sum = IntStream.of(numbers).sum();
+        //Average
+        double avg = IntStream.of(numbers).average().getAsDouble();
+
+        // Printing Smallest 3 distict numbers
+        IntStream.of(numbers)
+                 .distinct()
+                 .sorted()
+                 .limit(3)
+                 .forEach(System.out::println);
+        
+    }
+}
--- DSA/Graphs/Graph.java
+++ DSA/Graphs/Graph.java
@@ -54,11 +54,19 @@ public class Graph {
       dfs(u);
     }
   }
+  ///////////////////////////////////// Adjacency List Maker////////////////////////////////////////////////
   public void addNode(int u) {
     if (!edges.containsKey(u)) {
       edges.put(u, new TreeSet<Integer>());
     }
   }
+  public void addEdge(int u, int v) {
+    addNode(u);
+    addNode(v);
+    edges.get(u).add(v);
+    edges.get(v).add(u);
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
   public void removeNode(int u) {
     if (!edges.containsKey(u)) {
       return;
@@ -68,12 +76,7 @@ public class Graph {
     }
     edges.remove(u);
   }
-  public void addEdge(int u, int v) {
-    addNode(u);
-    addNode(v);
-    edges.get(u).add(v);
-    edges.get(v).add(u);
-  }
+  
 
   public void removeEdge(int u, int v) {
     edges.get(u).remove(v);
