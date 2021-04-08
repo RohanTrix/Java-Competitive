94e5dc10379b81a08dc2d07af1b842a9a406b3b8
RohanTrix <rohanyeahyeah@gmail.com>
2021-04-08 14:39:11 +0530

    Added Binary Search Code

---
+++ DSA/BinarySearch/Binary_Search.java
@@ -0,0 +1,27 @@
+package BinarySearch;
+
+public class Binary_Search {
+    public static void main(String args[])
+    {
+
+    }
+    int binarySearch(int[] nums, int target){
+        if(nums == null || nums.length == 0)
+          return -1;
+      
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+          // Prevent (left + right) overflow
+          int mid = left + (right - left) / 2;
+          if(nums[mid] == target)
+              return mid;
+          else if(nums[mid] < target) 
+              left = mid + 1; 
+          else 
+              right = mid - 1; 
+        }
+      
+        // End Condition: left > right
+        return -1;
+      }
+}
