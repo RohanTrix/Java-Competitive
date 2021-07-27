d11bab0d6f9a3c09f4a8c74393cdb526af96b778
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2021-07-27 12:52:34 +0530

    Added MP - LPS array and no. of strings matched

--- DSA/String/KMP.java
+++ DSA/String/KMP.java
@@ -1,4 +1,7 @@
 import java.util.*;
+
+// REFER: Zenith CP Course-  String Algortihms
+
 public class KMP {
     public static int[] getLPS(String s)
     {
@@ -16,11 +19,28 @@ public class KMP {
         }
         return lps;
     }
+    public static int matchSubstring(String s, String pat)
+    {
+        if(pat.length()==0) return 0;
+
+        int pat_len = pat.length();
+        String str = pat + "#" + s;
+
+        int lps[] = getLPS(str);
+        int cnt = 0;
+
+        for(int i =pat_len+1; i<str.length(); i++)
+        {
+            if(lps[i]==pat_len)cnt++;
+        }
+        return cnt;
+    }
     public static void main(String args[])
     {
         String s = "ababbaabab";
+        String pat = "ba";
         int LPS[] = getLPS(s);
         System.out.println(Arrays.toString(LPS));
-        
+        System.out.println(matchSubstring(s, pat));
     }
 }
